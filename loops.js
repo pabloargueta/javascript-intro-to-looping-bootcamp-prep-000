442bc8f16ce278e5a66b47920d5aa192e9d9a8ad
Pablo Argueta <pabloargueta27@gmail.com>
2018-05-04 02:48:49 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,3 +15,13 @@ function whileLoop(n) {
   }
   return 'done';
 }
+
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(array) {
+  do {
+    
+  } while (maybeTrue());
+}
