03042080a41ee83a607b6effff388f54ecc271af
Pablo Argueta <pabloargueta27@gmail.com>
2018-05-04 03:04:09 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,10 +2,10 @@ function forLoop(array){
   var i = 0;
   for (i; i < 25; i++){
     if (i === 1){
-      array.push(`I am ${i} strange loop.`);
+      array.push(`I am ${i} strange loop`);
     }
     else {
-      array.push(`I am ${i} strange loops.`);
+      array.push(`I am ${i} strange loops`);
     }
   }
 }
